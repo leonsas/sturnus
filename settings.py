246c2d09d1cbc8b973ba95afd530e819ffff9c74
unknown <leonsas@leonsas-HP.(none)>
2012-07-23 19:01:10 -0500

    changed template dir on settings

--- settings.py
+++ settings.py
@@ -104,7 +104,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'app.urls'
 
 TEMPLATE_DIRS = (
-'C:/Users/leonsas/Desktop/dj/sturnus/templates',
+os.path.join(ROOT_PATH,'/templates'),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
