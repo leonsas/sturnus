9d7ca9d7c80738c2beb93de157e4e365cce33faa
unknown <leonsas@leonsas-HP.(none)>
2012-07-27 12:00:48 -0500

    changed everything to js

--- friendgrapher.py
+++ friendgrapher.py
@@ -1,5 +1,6 @@
 import tweepy
 import simplejson as json
+from geopy import geocoders
 
 #param id can be string or int
 def getFriends(id):
@@ -7,27 +8,34 @@ def getFriends(id):
 	list_users=q[0]
 	print "Getting %s friends.." % (str(id))
 	ct=0
+	#loop through all the pages of twitter (max 100 users per request)
 	while len(q[0])>0:
 		print "Cursor counter at: %d" % (ct)
 		ct= ct+1
-		q=tweepy.api.friends(user_id=id,cursor=q[1][1])
+		q=tweepy.api.friends(user_id=id,cursor=q[1][1]) #cursor is q[1]
 		list_users=list_users+q[0]
+	
 	list_dict_users=[]
 	#add input id to the user list in index 0
 	this_user=tweepy.api.get_user(id)
+	loc=get_user_location(this_user)
 	list_dict_users.append({"id": this_user.id,
 								"screen_name": this_user.screen_name,
 								"name": this_user.name,
 								"followers_count": this_user.followers_count,
 								"friends_count": this_user.friends_count,
-								"statuses_count": this_user.statuses_count})
+								"statuses_count": this_user.statuses_count,
+								"location": loc,
+								})
 	for i in list_users:
+		loc=get_user_location(i)
 		list_dict_users.append({"id": i.id,
 								"screen_name": i.screen_name,
 								"name": i.name,
 								"followers_count": i.followers_count,
 								"friends_count": i.friends_count,
-								"statuses_count": i.statuses_count})
+								"statuses_count": i.statuses_count,
+								"location":loc})
 	return list_dict_users
 
 def create_dataset(list_users,output_form=0):
@@ -40,3 +48,16 @@ def create_dataset(list_users,output_form=0):
 		json_dict  = { "nodes" : list_users, "links" : list_links}
 	
 	return json.dumps(json_dict)
+
+def get_user_location(user):
+		g=geocoders.Google()
+		try:
+			location = g.geocode(user.location,exactly_one=False)
+			
+		except:
+			location = [None]
+		try:
+			print user.name,location
+		except:
+			print "this user probably has a weird character"
+		return location[0]
--- views.py
+++ views.py
@@ -24,7 +24,9 @@ def home(request,screenname=None):
 	
 def json_server(request, user_id=None):
     print "getting info: %s" % (str(user_id))
+    #get all the info from the friends, and send it as a json
     retStr=str(friendgrapher.create_dataset(friendgrapher.getFriends(user_id)))
+    #retStr="{\"locs\":[{\"lat\":0,\"lng\":0},{\"lat\":6,\"lng\":61},{\"lat\":39,\"lng\":31}]}"
     return HttpResponse(retStr)
 		
 
@@ -33,5 +35,22 @@ def json_builder(request,user_id):
 	return render_to_response('force-no-links.html',
 							   {'user_id':user_id,
 							   },
+							 context_instance=RequestContext(request)
+							)
+def map_test(request, screenname=None):
+	print "map_test"
+	
+	
+	return render_to_response('maps.html',
+                                   
+							 context_instance=RequestContext(request)
+							)
+							
+def testing_twitlib(request):
+	return render_to_response('twitlib.html',
+							 context_instance=RequestContext(request)
+							)
+def usingjs_view(request):
+	return render_to_response('bubble-js.html',
 							 context_instance=RequestContext(request)
 							)
